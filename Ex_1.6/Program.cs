fe2d4ff926ff1a8cdd08f3cfddc8b9db5e7f3013
kust <vad.velikanov@yandex.ru>
2023-10-06 16:48:34 +0300

    Add project files.

---
+++ Ex_1.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+double L = 0;
+double[] array = new double[5];
+
+Console.WriteLine("Введите значения");
+
+for (int i = 0; i <= 4; i++)
+{
+    array[i] = double.Parse(Console.ReadLine());
+    L += Math.Pow(array[i], 2);
+}
+Console.WriteLine(Math.Sqrt(L));
