a9b51adbc945028d40355861f808b543a39b5341
kust <vad.velikanov@yandex.ru>
2023-10-06 17:44:07 +0300

    exercise 11 (level 1)

---
+++ Ex_1.11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int[] array = new int[10];
+int count = 0;
+
+Console.WriteLine("Введите значение массива");
+for  (int i = 0; i <= 9; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел: {count}");
