351329a8a7db4ab497288cfbde803de604a7fe9e
kust <vad.velikanov@yandex.ru>
2023-10-09 19:38:14 +0300

    exercise 12 (level 1)

---
+++ Ex_1.12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int[] array = new int[8];
+int last = 0;
+int ind = 0;
+
+Console.WriteLine("Введите значения массива");
+for (int i = 0; i <= 7; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    if (array[i] < 0)
+    {
+        last = array[i];
+        ind = i+1;
+    }
+}
+Console.WriteLine($"Число {last}, его индекс: {ind}");
