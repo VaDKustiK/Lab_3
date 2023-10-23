68a939f0a7c638c692dd64c8c515c7fb4269b880
kust <vad.velikanov@yandex.ru>
2023-10-23 21:18:27 +0300

    exercise 10 (level 2)

---
+++ Ex_2.10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введите длину массива");
+int x = int.Parse(Console.ReadLine());
+int[] num = new int[x];
+Console.WriteLine("Введите значения массива");
+for (int i = 0; i < num.Length; i++)
+{
+    num[i] = int.Parse(Console.ReadLine());
+}
+
+int mini = 10000;
+for (int i = 0; i < x; i++)
+{
+    if (num[i] > 0 && num[i] < mini)
+    {
+        mini = num[i];
+    }
+}
+
+int[] num2 = new int[x-1];
+int j = 0;
+for (int i = 0; i < x; i++)
+{
+    if (num[i] != mini)
+    {
+        num2[j] = num[i];
+        j++;
+    }
+}
+
+Console.WriteLine(string.Join(", ", num2));
+Console.WriteLine($"удаленный элемент: {mini}");
