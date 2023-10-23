7abf2d9422fa63bfe711feea891abe0e06988849
kust <vad.velikanov@yandex.ru>
2023-10-23 21:40:25 +0300

    exercise 13 (level 2)

---
+++ Ex_2.13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введите длину массива");
+int x = int.Parse(Console.ReadLine());
+int[] num = new int[x];
+Console.WriteLine("Введите значения массива");
+for (int i = 0; i < num.Length; i++)
+{
+    num[i] = int.Parse(Console.ReadLine());
+}
+
+int max = num[0];
+int maxi = 0;
+for (int i = 0; i < x; i += 2)
+{
+    if (num[i] > max)
+    {
+        max = num[i];
+        maxi = i;
+    }
+}
+
+if (maxi % 2 == 0)
+{
+    num[maxi] = maxi;
+}
+Console.WriteLine(string.Join(", ", num));
