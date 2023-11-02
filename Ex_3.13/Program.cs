6de07f286a9aa474c92a5fe09f9766df16a0a059
kust <vad.velikanov@yandex.ru>
2023-11-02 20:07:59 +0300

    exercise 13 (level 3)

---
+++ Ex_3.13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int[] array = new int[12];
+Console.WriteLine("Введите значения массива");
+for (int i = 0; i < 12; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+int count = 0;
+for (int i = 0; i < 12 - 1; i++)
+{
+    for (int j = i + 1; j < 12 - count;)
+    {
+        if (array[i] == array[j])
+        {
+            count++;
+            for (int k = j; k < 11; k++)
+            {
+                array[k] = array[k + 1];
+            }
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+
+Console.WriteLine("Новые значения");
+for (int i = 0; i < 12 - count; i++)
+{
+    Console.WriteLine($"{array[i]}");
+}
